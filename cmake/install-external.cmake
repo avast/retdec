96bf8c7da523889c56e4ab2b54e41a37e3a09ca9
Michal Bandzi <michal.bandzi@avast.com>
2018-01-30 18:25:13 +0100

    add yara rules compilation for windows

--- cmake/install-external.cmake
+++ cmake/install-external.cmake
@@ -7,11 +7,22 @@ install(CODE "
 	if(INSTALL_SHARE_RES)
 		message(FATAL_ERROR \"RetDec share directory installation FAILED\")
 	endif()
-	execute_process(
-		COMMAND sh \"${CMAKE_SOURCE_DIR}/cmake/compile-yara.sh\" \"${YARAC_PATH}\" \"${CMAKE_SOURCE_DIR}\" \"${CMAKE_INSTALL_PREFIX}\"
-		RESULT_VARIABLE COMPILE_YARA_RES
-	)
-	if(COMPILE_YARA_RES)
-		message(FATAL_ERROR \"Yara tool signatures compilation FAILED\")
+	if(${MSVC})
+		execute_process(
+			COMMAND sh \"${CMAKE_SOURCE_DIR}/cmake/compile-yara.sh\" \"${YARAC_DIR}/\${CMAKE_INSTALL_CONFIG_NAME}/${YARAC_NAME}\" \"${CMAKE_SOURCE_DIR}\" \"${CMAKE_INSTALL_PREFIX}\"
+			COMMAND sh \"${CMAKE_SOURCE_DIR}/cmake/compile-yara.sh\" \"${YARAC_DIR}/\${CMAKE_INSTALL_CONFIG_NAME}/${YARAC_NAME}\" \"${CMAKE_SOURCE_DIR}\" \"${CMAKE_INSTALL_PREFIX}\"
+			RESULT_VARIABLE COMPILE_YARA_RES
+		)
+		if(COMPILE_YARA_RES)
+			message(FATAL_ERROR \"Yara tool signatures compilation FAILED\")
+		endif()
+	else()
+		execute_process(
+			COMMAND sh \"${CMAKE_SOURCE_DIR}/cmake/compile-yara.sh\" \"${YARAC_PATH}\" \"${CMAKE_SOURCE_DIR}\" \"${CMAKE_INSTALL_PREFIX}\"
+			RESULT_VARIABLE COMPILE_YARA_RES
+		)
+		if(COMPILE_YARA_RES)
+			message(FATAL_ERROR \"Yara tool signatures compilation FAILED\")
+		endif()
 	endif()
 ")
