avast/retdec

commit 7b5289acda02cfb8fbbf3a618c5a57c3f9c5a2a2

scripts: Fix the used sed commands at the end of retdec-decompiler.sh to work on macOS (#257).
The original commands failed with the following error on macOS:

    sed: 1: "/private/tmp/test.c": undefined label 'mp/test.c'
    sed: 1: "/private/tmp/test.c": undefined label 'mp/test.c'

The reason why the commands failed is that when using the -i parameter of sed
on macOS, you need to specify an extension of the backup file. If you do not
want to create a backup file, you have to pass the empty string. On Linux,
using just -i without any extension suffices.

The new sed command should work on Linux, macOS, and Windows, without a need to
install gnu-sed on macOS.

scripts/retdec-decompiler.sh
@@ -953,8 +953,9 @@ fi
 # Remove trailing whitespace and the last redundant empty new line from the
 # generated output (if any). It is difficult to do this in the back-end, so we
 # do it here.
-sed -i 's/[ \t]*$//' "$OUT"
-sed -i '$ { /^$/ d}' "$OUT"
+# Note: When modifying the command below, ensure that it works also on macOS as
+#       its sed differs from the GNU sed.
+sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba' -e 's/[[:space:]]*$//' -i'' -- "$OUT"
 
 # Colorize output file.
 if [ "$COLOR_IDA" ]; then

commit 11610c5ed3b726a2bbaa406ce81c1d5e60cafaaf

scripts: Prevent retdec-decompiler.sh from creating output.c-- files.
This fixes the used sed command from commit
7b5289acda02cfb8fbbf3a618c5a57c3f9c5a2a2, which caused "output.c--" files to be
created when running the decompiler on macOS.

Apparently, there is no way of getting sed -i to work consistently on both
MacOS and Linux. Indeed, this works with GNU sed, but not on macOS:

    sed -i 's/foo/bar/' FILE
    sed -i'' 's/foo/bar/' FILE

This works on macOS, but not with GNU sed:

    sed -i '' 's/foo/bar/' FILE

So, store the output from sed into a temporary file and rename that file
afterwards to get consistent behavior between macOS and Linux.

scripts/retdec-decompiler.sh
@@ -964,9 +964,10 @@ fi
 # Remove trailing whitespace and the last redundant empty new line from the
 # generated output (if any). It is difficult to do this in the back-end, so we
 # do it here.
-# Note: When modifying the command below, ensure that it works also on macOS as
-#       its sed differs from the GNU sed.
-sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba' -e 's/[[:space:]]*$//' -i'' -- "$OUT"
+# Note: Do not use the -i flag (in-place replace) as there is apparently no way
+#       of getting sed -i to work consistently on both MacOS and Linux.
+sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba' -e 's/[[:space:]]*$//' < "$OUT" > "$OUT.tmp"
+mv "$OUT.tmp" "$OUT"
 
 # Colorize output file.
 if [ "$COLOR_IDA" ]; then