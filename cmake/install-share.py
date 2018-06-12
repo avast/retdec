ed45adcc34efb9baf1cf6552beebb5ade9b5f4b9
Michael Kösel <michaelkoesel@gmx.de>
2018-06-12 21:42:26 +0200

    Fix compile for older python version

--- cmake/install-share.py
+++ cmake/install-share.py
@@ -8,7 +8,7 @@ import os
 import shutil
 import sys
 import tarfile
-import urllib.request
+import urllib
 
 # Check arguments.
 if len(sys.argv) != 2:
@@ -72,8 +72,8 @@ arch_url = 'https://github.com/avast-tl/retdec-support/releases/download/%s/%s' 
 print('Downloading archive from %s ...' % arch_url)
 
 try:
-    urllib.request.urlretrieve(arch_url, arch_path)
-except (urllib.request.HTTPError, urllib.request.URLError):
+    urllib.urlretrieve(arch_url, arch_path)
+except IOError:
     print('ERROR: download failed')
     cleanup()
     sys.exit(1)
