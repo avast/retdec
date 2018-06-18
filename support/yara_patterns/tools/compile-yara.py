c1635da991c4c1a55304c8978c1599d5265241b5
Michael Kösel <michaelkoesel@gmx.de>
2018-06-18 13:41:49 +0200

    [skip ci] Use correct code style

--- support/yara_patterns/tools/compile-yara.py
+++ support/yara_patterns/tools/compile-yara.py
@@ -12,7 +12,7 @@ import sys
 
 ###############################################################################
 
-def printErrorAndDie(error):
+def print_error_and_die(error):
     """Print error and exit with non-zero value.
          error - error message.
     """
@@ -20,7 +20,7 @@ def printErrorAndDie(error):
     sys.exit(1)
 
 
-def compileFiles(input_folder, output_folder):
+def compile_files(input_folder, output_folder):
     """Compile yara signatures.
         input_folder - input folder
         output_folder - output file
@@ -30,26 +30,26 @@ def compileFiles(input_folder, output_folder):
     out, _ = p.communicate()
 
     if p.returncode != 0:
-        printErrorAndDie('yarac failed during compilation of file' + input_folder)
+        print_error_and_die('yarac failed during compilation of file' + input_folder)
 
     # Check for errors in output - yarac returns 0 when it should not.
     if 'error' in out:
-        printErrorAndDie('yarac failed during compilation of file ' + input_folder)
+        print_error_and_die('yarac failed during compilation of file ' + input_folder)
 
 
 ###############################################################################
 
 if len(sys.argv) < 2:
-    printErrorAndDie('missing path to yarac')
+    print_error_and_die('missing path to yarac')
 yarac = sys.argv[1]
 
 if len(sys.argv) < 3:
-    printErrorAndDie('missing path to rules folder')
+    print_error_and_die('missing path to rules folder')
 
 rules_dir = sys.argv[2]
 
 if len(sys.argv) < 4:
-    printErrorAndDie('missing path to install folder')
+    print_error_and_die('missing path to install folder')
 
 install_dir = sys.argv[3]
 
@@ -77,32 +77,32 @@ os.makedirs(os.path.join(install_dir, 'macho'), exist_ok=True)
 print('compiling yara signatures...')
 
 # Compile PE32 signatures.
-compileFiles(os.path.join(rules_dir, 'pe', 'x86'), os.path.join(install_dir, 'pe', 'x86.yarac'))
-compileFiles(os.path.join(rules_dir, 'pe', 'arm'), os.path.join(install_dir, 'pe', 'arm.yarac'))
+compile_files(os.path.join(rules_dir, 'pe', 'x86'), os.path.join(install_dir, 'pe', 'x86.yarac'))
+compile_files(os.path.join(rules_dir, 'pe', 'arm'), os.path.join(install_dir, 'pe', 'arm.yarac'))
 
 # Compile PE32+ signatures.
-compileFiles(os.path.join(rules_dir, 'pe', 'x64'), os.path.join(install_dir, 'pe', 'x64.yarac'))
+compile_files(os.path.join(rules_dir, 'pe', 'x64'), os.path.join(install_dir, 'pe', 'x64.yarac'))
 
 # Compile ELF signatures.
-compileFiles(os.path.join(rules_dir, 'elf', 'x86'), os.path.join(install_dir, 'elf', 'x86.yarac'))
-compileFiles(os.path.join(rules_dir, 'elf', 'arm'), os.path.join(install_dir, 'elf', 'arm.yarac'))
-compileFiles(os.path.join(rules_dir, 'elf', 'ppc'), os.path.join(install_dir, 'elf', 'ppc.yarac'))
-compileFiles(os.path.join(rules_dir, 'elf', 'mips'), os.path.join(install_dir, 'elf', 'mips.yarac'))
+compile_files(os.path.join(rules_dir, 'elf', 'x86'), os.path.join(install_dir, 'elf', 'x86.yarac'))
+compile_files(os.path.join(rules_dir, 'elf', 'arm'), os.path.join(install_dir, 'elf', 'arm.yarac'))
+compile_files(os.path.join(rules_dir, 'elf', 'ppc'), os.path.join(install_dir, 'elf', 'ppc.yarac'))
+compile_files(os.path.join(rules_dir, 'elf', 'mips'), os.path.join(install_dir, 'elf', 'mips.yarac'))
 
 # Compile ELF64 signatures.
-compileFiles(os.path.join(rules_dir, 'elf', 'x64'), os.path.join(install_dir, 'elf', 'x64.yarac'))
-compileFiles(os.path.join(rules_dir, 'elf', 'arm64'), os.path.join(install_dir, 'elf', 'arm64.yarac'))
-compileFiles(os.path.join(rules_dir, 'elf', 'ppc64'), os.path.join(install_dir, 'elf', 'ppc64.yarac'))
-compileFiles(os.path.join(rules_dir, 'elf', 'mips64'), os.path.join(install_dir, 'elf', 'mips64.yarac'))
+compile_files(os.path.join(rules_dir, 'elf', 'x64'), os.path.join(install_dir, 'elf', 'x64.yarac'))
+compile_files(os.path.join(rules_dir, 'elf', 'arm64'), os.path.join(install_dir, 'elf', 'arm64.yarac'))
+compile_files(os.path.join(rules_dir, 'elf', 'ppc64'), os.path.join(install_dir, 'elf', 'ppc64.yarac'))
+compile_files(os.path.join(rules_dir, 'elf', 'mips64'), os.path.join(install_dir, 'elf', 'mips64.yarac'))
 
 # Compile Mach-O signatures.
-compileFiles(os.path.join(rules_dir, 'macho', 'x86'), os.path.join(install_dir, 'macho', 'x86.yarac'))
-compileFiles(os.path.join(rules_dir, 'macho', 'arm'), os.path.join(install_dir, 'macho', 'arm.yarac'))
-compileFiles(os.path.join(rules_dir, 'macho', 'ppc'), os.path.join(install_dir, 'macho', 'ppc.yarac'))
+compile_files(os.path.join(rules_dir, 'macho', 'x86'), os.path.join(install_dir, 'macho', 'x86.yarac'))
+compile_files(os.path.join(rules_dir, 'macho', 'arm'), os.path.join(install_dir, 'macho', 'arm.yarac'))
+compile_files(os.path.join(rules_dir, 'macho', 'ppc'), os.path.join(install_dir, 'macho', 'ppc.yarac'))
 
 # Compile 64-bit Mach-O signatures.
-compileFiles(os.path.join(rules_dir, 'macho', 'x64'), os.path.join(install_dir, 'macho', 'x64.yarac'))
-compileFiles(os.path.join(rules_dir, 'macho', 'ppc64'), os.path.join(install_dir, 'macho', 'ppc64.yarac'))
+compile_files(os.path.join(rules_dir, 'macho', 'x64'), os.path.join(install_dir, 'macho', 'x64.yarac'))
+compile_files(os.path.join(rules_dir, 'macho', 'ppc64'), os.path.join(install_dir, 'macho', 'ppc64.yarac'))
 
 print('signatures compiled successfully')
 sys.exit(0)
