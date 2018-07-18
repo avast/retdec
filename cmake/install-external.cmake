0938102924a6f0843da21d25ca15b69c1d9e588b
Peter Matula <peter.matula@avast.com>
2018-07-18 15:27:50 +0200

    cmake/install-share.py: refactor code and its usage in CMake.

--- cmake/install-external.cmake
+++ cmake/install-external.cmake
@@ -1,16 +1,9 @@
 install(CODE "
-	if (WIN32)
-		execute_process(
-			COMMAND py -3 \"${CMAKE_SOURCE_DIR}/cmake/install-share.py\" \"${CMAKE_INSTALL_PREFIX}\"
-			RESULT_VARIABLE INSTALL_SHARE_RES
-		)
-	else()
-		execute_process(
-			COMMAND python3 \"${CMAKE_SOURCE_DIR}/cmake/install-share.py\" \"${CMAKE_INSTALL_PREFIX}\"
-			RESULT_VARIABLE INSTALL_SHARE_RES
-		)
-	endif()
-	
+	execute_process(
+		# -u = unbuffered -> print debug messages right away.
+		COMMAND ${PYTHON3_EXECUTABLE} -u \"${CMAKE_SOURCE_DIR}/cmake/install-share.py\" \"${CMAKE_INSTALL_PREFIX}\"
+		RESULT_VARIABLE INSTALL_SHARE_RES
+	)
 	if(INSTALL_SHARE_RES)
 		message(FATAL_ERROR \"RetDec share directory installation FAILED\")
 	endif()
