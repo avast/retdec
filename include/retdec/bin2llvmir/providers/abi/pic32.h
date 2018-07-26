2be2dbb1e2ed6a93691fef1bd85c17a2556ee701
Peter Kubov <peter.kubov@avast.com>
2018-07-26 12:45:00 +0200

    abi: pic32: new class
    Provides special ABI for Pic32. This type of Mips
    architecture passes parameters differently
    than regular Mips and to reflect this we should
    have separated Pic32 ABI that would be suitable
    for other changes that my emerge by deompilation
    of sufficient Pic32 binary files.

---
+++ include/retdec/bin2llvmir/providers/abi/pic32.h
@@ -0,0 +1,37 @@
+/**
+ * @file include/retdec/bin2llvmir/providers/abi/mips.h
+ * @brief ABI information for MIPS.
+ * @copyright (c) 2017 Avast Software, licensed under the MIT license
+ */
+
+#ifndef RETDEC_BIN2LLVMIR_PROVIDERS_ABI_PIC32_H
+#define RETDEC_BIN2LLVMIR_PROVIDERS_ABI_PIC32_H
+
+#include "retdec/bin2llvmir/providers/abi/abi.h"
+
+namespace retdec {
+namespace bin2llvmir {
+
+class AbiPic32 : public Abi
+{
+	// Ctors, dtors.
+	//
+	public:
+		AbiPic32(llvm::Module* m, Config* c);
+		virtual ~AbiPic32();
+
+	// Registers.
+	//
+	public:
+		virtual bool isGeneralPurposeRegister(const llvm::Value* val) override;
+
+	// Instructions.
+	//
+	public:
+		virtual bool isNopInstruction(cs_insn* insn) override;
+};
+
+} // namespace bin2llvmir
+} // namespace retdec
+
+#endif
---
+++ src/bin2llvmir/providers/abi/pic32.cpp
@@ -0,0 +1,81 @@
+/**
+ * @file src/bin2llvmir/providers/abi/mips.h
+ * @brief ABI information for MIPS.
+ * @copyright (c) 2017 Avast Software, licensed under the MIT license
+ */
+
+#include "retdec/bin2llvmir/providers/abi/pic32.h"
+
+using namespace llvm;
+
+namespace retdec {
+namespace bin2llvmir {
+
+AbiPic32::AbiPic32(llvm::Module* m, Config* c) :
+		Abi(m, c)
+{
+	_regs.reserve(MIPS_REG_ENDING);
+	_id2regs.resize(MIPS_REG_ENDING, nullptr);
+	_regStackPointerId = MIPS_REG_SP;
+	_regZeroReg = MIPS_REG_ZERO;
+
+	// system calls
+	_regSyscallId = MIPS_REG_V0;
+	_regSyscallReturn = MIPS_REG_V0;
+	_syscallRegs = {
+			MIPS_REG_A0,
+			MIPS_REG_A1,
+			MIPS_REG_A2,
+			MIPS_REG_A3};
+
+	_regReturn = MIPS_REG_V0;
+	_regFPReturn = MIPS_REG_V0;
+
+	if (_config->getConfig().tools.isPspGcc())
+	{
+		_paramRegs = {
+			MIPS_REG_A0,
+			MIPS_REG_A1,
+			MIPS_REG_A2,
+			MIPS_REG_A3,
+			MIPS_REG_T0,
+			MIPS_REG_T1,
+			MIPS_REG_T2,
+			MIPS_REG_T3};
+	}
+	else
+	{
+		_paramRegs = {
+			MIPS_REG_A0,
+			MIPS_REG_A1,
+			MIPS_REG_A2,
+			MIPS_REG_A3};
+	}
+}
+
+AbiPic32::~AbiPic32()
+{
+
+}
+
+bool AbiPic32::isGeneralPurposeRegister(const llvm::Value* val)
+{
+	uint32_t rid = getRegisterId(val);
+	return MIPS_REG_0 <= rid && rid <= MIPS_REG_31;
+}
+
+bool AbiPic32::isNopInstruction(cs_insn* insn)
+{
+	// True NOP variants.
+	//
+	if (insn->id == MIPS_INS_NOP
+			|| insn->id == MIPS_INS_SSNOP)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+} // namespace bin2llvmir
+} // namespace retdec
