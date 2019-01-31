d5f629689111c42c5f4c76b7ab06314eddd1edbe
Matej Kastak <matej.kastak@gmail.com>
2019-01-31 13:48:03 +0100

    Changed register generation to match other modules.

--- src/capstone2llvmir/arm64/arm64.cpp
+++ src/capstone2llvmir/arm64/arm64.cpp
@@ -68,98 +68,10 @@ void Capstone2LlvmIrTranslatorArm64_impl::generateDataLayout()
 
 void Capstone2LlvmIrTranslatorArm64_impl::generateRegisters()
 {
-	// General purpose registers.
-	//
-	createRegister(ARM64_REG_X0, _regLt);
-	createRegister(ARM64_REG_X1, _regLt);
-	createRegister(ARM64_REG_X2, _regLt);
-	createRegister(ARM64_REG_X3, _regLt);
-	createRegister(ARM64_REG_X4, _regLt);
-	createRegister(ARM64_REG_X5, _regLt);
-	createRegister(ARM64_REG_X6, _regLt);
-	createRegister(ARM64_REG_X7, _regLt);
-	createRegister(ARM64_REG_X8, _regLt);
-	createRegister(ARM64_REG_X9, _regLt);
-	createRegister(ARM64_REG_X10, _regLt);
-	createRegister(ARM64_REG_X11, _regLt);
-	createRegister(ARM64_REG_X12, _regLt);
-	createRegister(ARM64_REG_X13, _regLt);
-	createRegister(ARM64_REG_X14, _regLt);
-	createRegister(ARM64_REG_X15, _regLt);
-	createRegister(ARM64_REG_X16, _regLt);
-	createRegister(ARM64_REG_X17, _regLt);
-	createRegister(ARM64_REG_X18, _regLt);
-	createRegister(ARM64_REG_X19, _regLt);
-	createRegister(ARM64_REG_X20, _regLt);
-	createRegister(ARM64_REG_X21, _regLt);
-	createRegister(ARM64_REG_X22, _regLt);
-	createRegister(ARM64_REG_X23, _regLt);
-	createRegister(ARM64_REG_X24, _regLt);
-	createRegister(ARM64_REG_X25, _regLt);
-	createRegister(ARM64_REG_X26, _regLt);
-	createRegister(ARM64_REG_X27, _regLt);
-	createRegister(ARM64_REG_X28, _regLt);
-
-	// Lower 32 bits of 64 arm{xN} bit regs.
-	//
-	/*
-	createRegister(ARM64_REG_W0, _regLt);
-	createRegister(ARM64_REG_W1, _regLt);
-	createRegister(ARM64_REG_W2, _regLt);
-	createRegister(ARM64_REG_W3, _regLt);
-	createRegister(ARM64_REG_W4, _regLt);
-	createRegister(ARM64_REG_W5, _regLt);
-	createRegister(ARM64_REG_W6, _regLt);
-	createRegister(ARM64_REG_W7, _regLt);
-	createRegister(ARM64_REG_W8, _regLt);
-	createRegister(ARM64_REG_W9, _regLt);
-	createRegister(ARM64_REG_W10, _regLt);
-	createRegister(ARM64_REG_W11, _regLt);
-	createRegister(ARM64_REG_W12, _regLt);
-	createRegister(ARM64_REG_W13, _regLt);
-	createRegister(ARM64_REG_W14, _regLt);
-	createRegister(ARM64_REG_W15, _regLt);
-	createRegister(ARM64_REG_W16, _regLt);
-	createRegister(ARM64_REG_W17, _regLt);
-	createRegister(ARM64_REG_W18, _regLt);
-	createRegister(ARM64_REG_W19, _regLt);
-	createRegister(ARM64_REG_W20, _regLt);
-	createRegister(ARM64_REG_W21, _regLt);
-	createRegister(ARM64_REG_W22, _regLt);
-	createRegister(ARM64_REG_W23, _regLt);
-	createRegister(ARM64_REG_W24, _regLt);
-	createRegister(ARM64_REG_W25, _regLt);
-	createRegister(ARM64_REG_W26, _regLt);
-	createRegister(ARM64_REG_W27, _regLt);
-	createRegister(ARM64_REG_W28, _regLt);
-	createRegister(ARM64_REG_W29, _regLt);
-	createRegister(ARM64_REG_W30, _regLt);
-	*/
-
-	// Special registers.
-
-	// FP Frame pointer.
-	createRegister(ARM64_REG_X29, _regLt);
-
-	// LP Link register.
-	createRegister(ARM64_REG_X30, _regLt);
-
-	// Stack pointer.
-	createRegister(ARM64_REG_SP, _regLt);
-	//createRegister(ARM64_REG_WSP, _regLt);
-
-	// Zero.
-	createRegister(ARM64_REG_XZR, _regLt);
-	//createRegister(ARM64_REG_WZR, _regLt);
-
-	// Flags.
-	createRegister(ARM64_REG_CPSR_N, _regLt);
-	createRegister(ARM64_REG_CPSR_Z, _regLt);
-	createRegister(ARM64_REG_CPSR_C, _regLt);
-	createRegister(ARM64_REG_CPSR_V, _regLt);
-
-	// Program counter.
-	createRegister(ARM64_REG_PC, _regLt);
+	for (auto& p : _reg2type)
+	{
+		createRegister(p.first, _regLt);
+	}
 }
 
 uint32_t Capstone2LlvmIrTranslatorArm64_impl::getCarryRegister()
@@ -536,7 +448,7 @@ llvm::Value* Capstone2LlvmIrTranslatorArm64_impl::loadRegister(
 	auto* llvmReg = getRegister(pr);
 	if (llvmReg == nullptr)
 	{
-		//throw Capstone2LlvmIrError("loadRegister() unhandled reg.");
+		throw GenericError("loadRegister() unhandled reg.");
 	}
 
 	llvm::Value* ret = irb.CreateLoad(llvmReg);
@@ -616,7 +528,7 @@ llvm::Instruction* Capstone2LlvmIrTranslatorArm64_impl::storeRegister(
 	auto* llvmReg = getRegister(pr);
 	if (llvmReg == nullptr)
 	{
-		//throw Capstone2LlvmIrError("storeRegister() unhandled reg.");
+		throw GenericError("storeRegister() unhandled reg.");
 	}
 
 	val = generateTypeConversion(irb, val, llvmReg->getValueType(), ct);
@@ -635,7 +547,7 @@ llvm::Instruction* Capstone2LlvmIrTranslatorArm64_impl::storeRegister(
 				|| l->getType()->isIntegerTy(32)
 				|| l->getType()->isIntegerTy(64)))
 		{
-			//throw Capstone2LlvmIrError("Unexpected parent type.");
+			throw GenericError("Unexpected parent type.");
 		}
 
 		llvm::Value* andC = nullptr;
