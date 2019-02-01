274ed7e2a89975c830e6aa1d568c315036b0b022
Peter Matula <peter.matula@avast.com>
2019-02-01 15:00:33 +0100

    llvmir2hll: use goto_stmt_optimizer.

--- src/llvmir2hll/optimizer/optimizers/goto_stmt_optimizer.cpp
+++ src/llvmir2hll/optimizer/optimizers/goto_stmt_optimizer.cpp
@@ -42,63 +42,23 @@ GotoStmtOptimizer::GotoStmtOptimizer(ShPtr<Module> module):
 GotoStmtOptimizer::~GotoStmtOptimizer() {}
 
 /**
-* TODO: prebrat s Petrom:
-* - vymazanie labelu, ak nan uz neskace ziadne goto. vyzera to tak, ze
-*   Statement::preds sa nedostatocne cisti. target ma v preds goto, ktore uz
-*   nie su v AST.
-*   politika remove/replace statment? vymazu sa tie statementy, alebo co sa s
-*   nimi stane? ako dokonale sa cistia/maju cistit preds/succ?
-* - opakovane spustenie tejto (inych?) analyz kym nie je dosiahnuty fixpoint? -> e.g. copy propagation
-* - pustenie tejto (inych) analyz niekolko nasobne? na roznych miestach? ku
-*   koncu?
-* - Auto cistenie targetu, preds, succ, etc v ~GotoStmt() a inych destruktoroch?
-* - goto target set, but succ nullptr.
+* TODO
 */
 void GotoStmtOptimizer::visit(ShPtr<GotoStmt> stmt) {
-
-
-std::cout << "====> this goto = " << getPtrStr(stmt.get()) << std::endl;
-
 	auto target = stmt->getTarget();
 	if (isa<GotoStmt>(target)
 			|| isa<ReturnStmt>(target)
 			|| isa<BreakStmt>(target)
 			|| isa<ContinueStmt>(target)) {
-		std::cout << "goto -> " << target->getLabel() << std::endl;
-
-//std::cout << "\t" << "succ   = " << getPtrStr(stmt->getSuccessor().get()) << std::endl; // = nullptr
-//std::cout << "\t" << "target = " << getPtrStr(stmt->getTarget().get()) << std::endl;    // = target ptr
-//return;
-
 		auto c = ucast<Statement>(target->clone());
 		c->setMetadata("");
 
 		stmt->prependStatement(c);
 		Statement::removeStatement(stmt);
 
-//		Statement::replaceStatement(stmt, c);
-
 		if (!target->isGotoTarget()) {
 			target->removeLabel();
-			std::cout << "\tremoving label: " << target->getLabel() << std::endl;
 		}
-
-		for (auto it = target->predecessor_begin(), e = target->predecessor_end(); it != e; ++it) {
-			auto pred = *it;
-
-			if (auto gotoStmt = cast<GotoStmt>(pred)) {
-				if (gotoStmt->getTarget() == target) {
-					std::cout << std::endl;
-					std::cout << "\t" << "this goto = " << getPtrStr(stmt.get())
-							<< ", " << stmt->getNumberOfPredecessors() << std::endl;
-					std::cout << "\t" << "pred goto = " << getPtrStr(gotoStmt.get())
-								<< ", " << gotoStmt->getNumberOfPredecessors() << std::endl;
-				}
-			}
-		}
-
-		// TODO: remove/delete stmt?
-		// Statement::removeStatement() does not seems to be doing that.
 	}
 }
 
