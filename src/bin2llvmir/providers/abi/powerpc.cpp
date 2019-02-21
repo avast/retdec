9b173746ce12b65ba9aa630578e15b55079d6df9
Peter Kubov <peter.kubov@avast.com>
2019-02-21 12:36:44 +0100

    abi/powerpc: revert default c cproviding

--- src/bin2llvmir/providers/abi/powerpc.cpp
+++ src/bin2llvmir/providers/abi/powerpc.cpp
@@ -17,19 +17,6 @@ AbiPowerpc::AbiPowerpc(llvm::Module* m, Config* c) :
 	_regs.reserve(PPC_REG_ENDING);
 	_id2regs.resize(PPC_REG_ENDING, nullptr);
 	_regStackPointerId = PPC_REG_R1;
-
-	_paramRegs = {
-		PPC_REG_R3,
-		PPC_REG_R4,
-		PPC_REG_R5,
-		PPC_REG_R6,
-		PPC_REG_R7,
-		PPC_REG_R8,
-		PPC_REG_R9,
-		PPC_REG_R10};
-
-	_regReturn = PPC_REG_R3;
-	_regFPReturn = PPC_REG_R3;
 }
 
 AbiPowerpc::~AbiPowerpc()
