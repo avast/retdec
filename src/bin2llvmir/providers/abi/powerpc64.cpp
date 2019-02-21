540415ebb0877f7a0c44ec4980d7aebf99d63593
Peter Kubov <peter.kubov@avast.com>
2019-02-21 12:39:10 +0100

    abi/powerpc64: provide default cc

--- src/bin2llvmir/providers/abi/powerpc64.cpp
+++ src/bin2llvmir/providers/abi/powerpc64.cpp
@@ -17,6 +17,8 @@ AbiPowerpc64::AbiPowerpc64(llvm::Module* m, Config* c) :
 	_regs.reserve(PPC_REG_ENDING);
 	_id2regs.resize(PPC_REG_ENDING, nullptr);
 	_regStackPointerId = PPC_REG_R1;
+
+	_defcc = CallingConvention::ID::CC_POWERPC64;
 }
 
 AbiPowerpc64::~AbiPowerpc64()
