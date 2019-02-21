95c63b7fcd0211cff2743b40e04e2950fc799e72
Peter Kubov <peter.kubov@avast.com>
2019-02-21 12:38:06 +0100

    abi/powerpc64: revert cc info

--- src/bin2llvmir/providers/abi/powerpc64.cpp
+++ src/bin2llvmir/providers/abi/powerpc64.cpp
@@ -17,36 +17,6 @@ AbiPowerpc64::AbiPowerpc64(llvm::Module* m, Config* c) :
 	_regs.reserve(PPC_REG_ENDING);
 	_id2regs.resize(PPC_REG_ENDING, nullptr);
 	_regStackPointerId = PPC_REG_R1;
-	_fpRegsAsParams = true;
-
-	_paramRegs = {
-		PPC_REG_R3,
-		PPC_REG_R4,
-		PPC_REG_R5,
-		PPC_REG_R6,
-		PPC_REG_R7,
-		PPC_REG_R8,
-		PPC_REG_R9,
-		PPC_REG_R10};
-
-	_paramFPRegs = {
-		PPC_REG_F1,
-		PPC_REG_F2,
-		PPC_REG_F3,
-		PPC_REG_F4,
-		PPC_REG_F5,
-		PPC_REG_F6,
-		PPC_REG_F7,
-		PPC_REG_F8,
-		PPC_REG_F9,
-		PPC_REG_F10,
-		PPC_REG_F11};
-
-	// TODO paramVectorRegs = 
-
-
-	_regReturn = PPC_REG_R3;
-	_regFPReturn = PPC_REG_R3;
 }
 
 AbiPowerpc64::~AbiPowerpc64()
