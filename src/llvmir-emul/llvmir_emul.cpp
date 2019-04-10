fac328a4f0709ce1521a3b8eca96d4dd0d3457cc
Peter Matula <peter.matula@avast.com>
2019-04-10 15:46:39 +0200

    llvmir-emul: remove "include <llvm/IR/TypeBuilder.h>".
    This header is no longer in LLVm 8.0.0.

--- src/llvmir-emul/llvmir_emul.cpp
+++ src/llvmir-emul/llvmir_emul.cpp
@@ -13,7 +13,6 @@
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Type.h>
-#include <llvm/IR/TypeBuilder.h>
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/Debug.h>
 #include <llvm/Support/DynamicLibrary.h>
