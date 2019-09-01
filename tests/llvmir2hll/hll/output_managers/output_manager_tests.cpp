6751a79071967c97def3bd096d84f9889fa60c5b
Peter Matula <peter.matula@avast.com>
2019-09-01 12:50:20 +0200

    llvmir2hll: add unit tests

---
+++ tests/llvmir2hll/hll/output_managers/output_manager_tests.cpp
@@ -0,0 +1,187 @@
+/**
+* @file tests/llvmir2hll/hll/output_managers/output_manager_tests.cpp
+* @brief Implementation of the base class for tests of output managers.
+* @copyright (c) 2019 Avast Software, licensed under the MIT license
+*/
+
+#include "llvmir2hll/hll/output_managers/output_manager_tests.h"
+#include "retdec/llvmir2hll/hll/output_managers/plain_manager.h"
+#include "retdec/utils/string.h"
+
+using namespace ::testing;
+
+using retdec::utils::contains;
+
+namespace retdec {
+namespace llvmir2hll {
+namespace tests {
+
+OutputManagerTests::OutputManagerTests() :
+		codeStream(code)
+{
+
+}
+
+void OutputManagerTests::SetUp()
+{
+	// By default, use PlainOutputManager to test functionality that is shared
+	// between output managers.
+	manager = UPtr<OutputManager>(new PlainOutputManager(codeStream));
+}
+
+std::string OutputManagerTests::emitCode()
+{
+	// We need to call the manager's destrutor to make sure all the code was
+	// emited.
+	manager.reset();
+	return codeStream.str();
+}
+
+//
+// comment prefix
+//
+
+TEST_F(OutputManagerTests, uninited_comment_prefix_is_empty)
+{
+	EXPECT_EQ("", manager->getCommentPrefix());
+}
+
+TEST_F(OutputManagerTests, init_comment_prefix)
+{
+	std::string prefix = "//";
+
+	manager->setCommentPrefix(prefix);
+
+	EXPECT_EQ(prefix, manager->getCommentPrefix());
+}
+
+//
+// output language
+//
+
+TEST_F(OutputManagerTests, uninited_language_is_empty)
+{
+	EXPECT_EQ("", manager->getOutputLanguage());
+}
+
+TEST_F(OutputManagerTests, init_language)
+{
+	std::string lang = "c";
+
+	manager->setOutputLanguage(lang);
+
+	EXPECT_EQ(lang, manager->getOutputLanguage());
+}
+
+//
+// operatorX()
+//
+
+TEST_F(OutputManagerTests, operatorX_no_spaces)
+{
+	manager->operatorX("++", false, false);
+
+	EXPECT_EQ("++", emitCode());
+}
+
+TEST_F(OutputManagerTests, operatorX_space_before)
+{
+	manager->operatorX("++", true, false);
+
+	EXPECT_EQ(" ++", emitCode());
+}
+
+TEST_F(OutputManagerTests, operatorX_space_after)
+{
+	manager->operatorX("++", false, true);
+
+	EXPECT_EQ("++ ", emitCode());
+}
+
+TEST_F(OutputManagerTests, operatorX_space_before_after)
+{
+	manager->operatorX("++", true, true);
+
+	EXPECT_EQ(" ++ ", emitCode());
+}
+
+//
+// comment()
+//
+
+TEST_F(OutputManagerTests, comment_without_indent)
+{
+	manager->setCommentPrefix("//");
+	manager->comment("hello world", "");
+
+	EXPECT_EQ("// hello world", emitCode());
+}
+
+TEST_F(OutputManagerTests, comment_with_indent)
+{
+	manager->setCommentPrefix("//");
+	manager->comment("hello world", "    ");
+
+	EXPECT_EQ("    // hello world", emitCode());
+}
+
+//
+// commentLine()
+//
+
+TEST_F(OutputManagerTests, comment_line_without_indent)
+{
+	manager->setCommentPrefix("//");
+	manager->commentLine("hello world");
+
+	EXPECT_EQ("// hello world\n", emitCode());
+}
+
+TEST_F(OutputManagerTests, comment_line_with_indent)
+{
+	manager->setCommentPrefix("//");
+	manager->commentLine("hello world", "    ");
+
+	EXPECT_EQ("    // hello world\n", emitCode());
+}
+
+//
+// includeLine()
+//
+
+TEST_F(OutputManagerTests, include_line_basic)
+{
+	manager->includeLine("stdlib.h");
+
+	EXPECT_EQ("#include <stdlib.h>\n", emitCode());
+}
+
+TEST_F(OutputManagerTests, include_line_basic_indent)
+{
+	manager->includeLine("stdlib.h", "    ");
+
+	EXPECT_EQ("    #include <stdlib.h>\n", emitCode());
+}
+
+TEST_F(OutputManagerTests, include_line_basic_indent_comment)
+{
+	manager->setCommentPrefix("//");
+	manager->includeLine("stdlib.h", "    ", "hello");
+
+	EXPECT_EQ("    #include <stdlib.h> // hello\n", emitCode());
+}
+
+//
+// typedefLine()
+//
+
+TEST_F(OutputManagerTests, typedef_line)
+{
+	manager->typedefLine("    ", "int", "uint64_t");
+
+	EXPECT_EQ("    typedef int uint64_t;\n", emitCode());
+}
+
+} // namespace tests
+} // namespace llvmir2hll
+} // namespace retdec
---
+++ tests/llvmir2hll/hll/output_managers/plain_manager_tests.cpp
@@ -0,0 +1,211 @@
+/**
+* @file tests/llvmir2hll/hll/output_managers/plain_manager_tests.cpp
+* @brief Implementation of class for tests of Plain output manager.
+* @copyright (c) 2019 Avast Software, licensed under the MIT license
+*/
+
+#include "llvmir2hll/hll/output_managers/output_manager_tests.h"
+#include "retdec/llvmir2hll/hll/output_managers/plain_manager.h"
+#include "retdec/utils/string.h"
+
+using namespace ::testing;
+
+using retdec::utils::contains;
+
+namespace retdec {
+namespace llvmir2hll {
+namespace tests {
+
+class PlainOutputManagerTests: public OutputManagerTests
+{
+	protected:
+		virtual void SetUp() override;
+};
+
+void PlainOutputManagerTests::SetUp()
+{
+	OutputManagerTests::SetUp();
+	manager = UPtr<OutputManager>(new PlainOutputManager(codeStream));
+	manager->setCommentPrefix("//");
+}
+
+//
+// tokens
+//
+
+TEST_F(PlainOutputManagerTests, token_newline)
+{
+	manager->newLine();
+	EXPECT_EQ("\n", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_space_default)
+{
+	manager->space();
+	EXPECT_EQ(" ", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_space_space)
+{
+	manager->space("    ");
+	EXPECT_EQ("    ", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_space_tab)
+{
+	manager->space("\t");
+	EXPECT_EQ("\t", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_punctuation)
+{
+	manager->punctuation(';');
+	EXPECT_EQ(";", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_operatorX)
+{
+	manager->operatorX("++");
+	EXPECT_EQ("++", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_variableId)
+{
+	manager->variableId("var");
+	EXPECT_EQ("var", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_memberId)
+{
+	manager->memberId("mem");
+	EXPECT_EQ("mem", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_labelId)
+{
+	manager->labelId("label");
+	EXPECT_EQ("label", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_functionId)
+{
+	manager->functionId("func");
+	EXPECT_EQ("func", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_parameterId)
+{
+	manager->parameterId("arg");
+	EXPECT_EQ("arg", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_keyword)
+{
+	manager->keyword("while");
+	EXPECT_EQ("while", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_dataType)
+{
+	manager->dataType("unsigned");
+	EXPECT_EQ("unsigned", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_preprocessor)
+{
+	manager->preprocessor("#define");
+	EXPECT_EQ("#define", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_include)
+{
+	manager->include("stdlib.h");
+	EXPECT_EQ("<stdlib.h>", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_constantBool)
+{
+	manager->constantBool("true");
+	EXPECT_EQ("true", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_constantInt)
+{
+	manager->constantInt("1234");
+	EXPECT_EQ("1234", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_constantFloat)
+{
+	manager->constantFloat("3.14");
+	EXPECT_EQ("3.14", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_constantString)
+{
+	manager->constantString("\"hello world\"");
+	EXPECT_EQ("\"hello world\"", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_constantSymbol)
+{
+	manager->constantSymbol("UNKNOWN");
+	EXPECT_EQ("UNKNOWN", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_constantPointer)
+{
+	manager->constantPointer("NULL");
+	EXPECT_EQ("NULL", emitCode());
+}
+
+TEST_F(PlainOutputManagerTests, token_comment)
+{
+	manager->comment("hello world");
+	EXPECT_EQ("// hello world", emitCode());
+}
+
+//
+// commentModifier()
+//
+
+TEST_F(PlainOutputManagerTests, commentModifier_creates_comment_until_end_of_line)
+{
+	manager->commentModifier();
+	manager->variableId("hello");
+	manager->space();
+	manager->operatorX("=");
+	manager->space();
+	manager->constantInt("1234");
+	manager->punctuation(';');
+	manager->newLine();
+	manager->functionId("ackermann");
+
+	EXPECT_EQ("// hello = 1234;\nackermann", emitCode());
+}
+
+//
+// addressPush()
+// addressPop()
+//
+
+TEST_F(PlainOutputManagerTests, address_push_and_pop_do_nothing)
+{
+	manager->addressPush(0x1000);
+	manager->variableId("hello");
+	manager->addressPush(0x1234);
+	manager->space();
+	manager->operatorX("=");
+	manager->space();
+	manager->addressPop();
+	manager->constantInt("1234");
+	manager->punctuation(';');
+	manager->addressPop();
+
+	EXPECT_EQ("hello = 1234;", emitCode());
+}
+
+
+} // namespace tests
+} // namespace llvmir2hll
+} // namespace retdec
