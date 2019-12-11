b010076fabfc96c19a9066b045528e05de46beda
Matula Peter <peter.matula@avast.com>
2019-12-11 15:23:32 +0100

    llvmir2hll: remove auxiliary variables optimizer.
    Everything that it did should be also performed by copy propagation optimizer.

--- src/llvmir2hll/optimizer/optimizer_manager.cpp
+++ src/llvmir2hll/optimizer/optimizer_manager.cpp
@@ -12,7 +12,6 @@
 #include "retdec/llvmir2hll/optimizer/optimizer_manager.h"
 #include "retdec/llvmir2hll/optimizer/optimizers/aggressive_deref_optimizer.h"
 #include "retdec/llvmir2hll/optimizer/optimizers/aggressive_global_to_local_optimizer.h"
-#include "retdec/llvmir2hll/optimizer/optimizers/auxiliary_variables_optimizer.h"
 #include "retdec/llvmir2hll/optimizer/optimizers/bit_op_to_log_op_optimizer.h"
 #include "retdec/llvmir2hll/optimizer/optimizers/bit_shift_optimizer.h"
 #include "retdec/llvmir2hll/optimizer/optimizers/break_continue_return_optimizer.h"
@@ -187,8 +186,6 @@ void OptimizerManager::optimize(ShPtr<Module> m) {
 	run<DeadLocalAssignOptimizer>(m, va);
 	run<SimpleCopyPropagationOptimizer>(m, va, cio);
 	run<CopyPropagationOptimizer>(m, va, cio);
-	// AuxiliaryVariablesOptimizer should be run after CopyPropagationOptimizer.
-	run<AuxiliaryVariablesOptimizer>(m, va, cio);
 
 	// SimplifyArithmExprOptimizer should be run before loop optimizations.
 	run<SimplifyArithmExprOptimizer>(m, arithmExprEvaluator);
