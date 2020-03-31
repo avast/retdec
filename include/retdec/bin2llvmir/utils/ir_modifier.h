5c3456fabc88acdb3280d63e91531b02eacd7cdb
Peter Matula <peter.matula@avast.com>
2020-03-31 19:55:35 +0200

    bin2llvmir/idioms_libgcc: remove reg localization from this pass.

--- include/retdec/bin2llvmir/utils/ir_modifier.h
+++ include/retdec/bin2llvmir/utils/ir_modifier.h
@@ -27,12 +27,6 @@ class IrModifier
 	// Can be used simply like this: \c IrModifier::method().
 	//
 	public:
-		template<typename Container>
-		static bool localize(
-				llvm::Instruction* storeDefinition,
-				const Container& uses,
-				bool eraseDefinition = true);
-
 		static llvm::AllocaInst* createAlloca(
 				llvm::Function* fnc,
 				llvm::Type* ty,
@@ -125,40 +119,6 @@ class IrModifier
 		llvm::Module* _module = nullptr;
 		Config* _config = nullptr;
 };
-
-template<typename Container>
-bool IrModifier::localize(
-		llvm::Instruction* storeDefinition,
-		const Container& uses,
-		bool eraseDefinition)
-{
-	llvm::StoreInst* definition = llvm::dyn_cast_or_null<llvm::StoreInst>(
-			storeDefinition);
-	if (definition == nullptr)
-	{
-		return false;
-	}
-	auto* ptr = definition->getPointerOperand();
-	auto* f = definition->getFunction();
-
-	auto* local = new llvm::AllocaInst(
-			ptr->getType()->getPointerElementType(),
-			Abi::DEFAULT_ADDR_SPACE);
-	local->insertBefore(&f->getEntryBlock().front());
-
-	new llvm::StoreInst(definition->getValueOperand(), local, definition);
-	if (eraseDefinition)
-	{
-		definition->eraseFromParent();
-	}
-
-	for (auto* u : uses)
-	{
-		reinterpret_cast<llvm::Instruction*>(u)->replaceUsesOfWith(ptr, local);
-	}
-
-	return true;
-}
 
 } // namespace bin2llvmir
 } // namespace retdec
