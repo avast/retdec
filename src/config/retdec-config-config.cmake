4e5be6205ea6ed4ec41993940326ad33de6dbb23
Peter Matula <imatula@fit.vutbr.cz>
2020-04-02 16:42:51 +0200

    config/retdec-config-config.cmake: fix required component order
    Looks like this may matter (on some machines or cmake version).

--- src/config/retdec-config-config.cmake
+++ src/config/retdec-config-config.cmake
@@ -1,5 +1,5 @@
 
 include(CMakeFindDependencyMacro)
-find_dependency(retdec 4.0 REQUIRED COMPONENTS common serdes utils)
+find_dependency(retdec 4.0 REQUIRED COMPONENTS common rapidjson serdes utils)
 
 include(${CMAKE_CURRENT_LIST_DIR}/retdec-config-targets.cmake)
