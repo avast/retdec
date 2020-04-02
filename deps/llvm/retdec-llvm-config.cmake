4521668189b6c14177251ecfff3be72573e6172a
Peter Matula <peter.matula@avast.com>
2020-04-02 10:01:38 +0200

    deps/llvm: refactor cmake to properly install libs

--- deps/llvm/retdec-llvm-config.cmake
+++ deps/llvm/retdec-llvm-config.cmake
@@ -1,7 +1,16 @@
+@PACKAGE_INIT@
 
 find_package(Threads REQUIRED)
 if(UNIX OR MINGW)
 	find_package(ZLIB REQUIRED)
 endif()
+
+add_library(llvm-libs INTERFACE)
+add_library(retdec::llvm-libs ALIAS llvm-libs)
+foreach(LLVM_LIB @PACKAGE_LLVM_LIBS_PATHS@)
+	target_link_libraries(llvm-libs INTERFACE
+		${LLVM_LIB}
+	)
+endforeach(LLVM_LIB)
 
 include(${CMAKE_CURRENT_LIST_DIR}/retdec-llvm-targets.cmake)
