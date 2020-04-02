0484d800a32d32bbb1f2959fbf3f9ffb31d1cbda
Peter Matula <peter.matula@avast.com>
2020-04-02 15:05:27 +0200

    deps/yaramod: refactor cmake to properly install libs

--- deps/yaramod/retdec-yaramod-config.cmake
+++ deps/yaramod/retdec-yaramod-config.cmake
@@ -1,2 +1,13 @@
+@PACKAGE_INIT@
+
+if(NOT TARGET yaramod-libs)
+    add_library(yaramod-libs INTERFACE)
+    add_library(retdec::yaramod-libs ALIAS yaramod-libs)
+    foreach(YARAMOD_LIB @PACKAGE_YARAMOD_LIBS_INSTALLED@)
+        target_link_libraries(yaramod-libs INTERFACE
+            ${YARAMOD_LIB}
+        )
+    endforeach(YARAMOD_LIB)
+endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/retdec-yaramod-targets.cmake)
