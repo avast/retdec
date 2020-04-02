97dc1693386825f99db20602cee34d4c9a6d666f
Peter Matula <peter.matula@avast.com>
2020-04-02 10:36:59 +0200

    deps/capstone: refactor cmake to properly install libs

--- deps/capstone/retdec-capstone-config.cmake
+++ deps/capstone/retdec-capstone-config.cmake
@@ -1,2 +1,8 @@
+@PACKAGE_INIT@
+
+add_library(retdec::capstone-libs STATIC IMPORTED)
+set_target_properties(retdec::capstone-libs PROPERTIES
+    IMPORTED_LOCATION @PACKAGE_CAPSTONE_LIB_INSTALLED@
+)
 
 include(${CMAKE_CURRENT_LIST_DIR}/retdec-capstone-targets.cmake)
