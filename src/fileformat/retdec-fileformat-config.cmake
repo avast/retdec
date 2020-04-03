e1645d7fd5e81d1606c96752da3ac633c38a63e1
Peter Matula <peter.matula@avast.com>
2020-04-03 16:31:07 +0200

    fileinfo: add openssl-crypto dep

--- src/fileformat/retdec-fileformat-config.cmake
+++ src/fileformat/retdec-fileformat-config.cmake
@@ -1,15 +1,16 @@
 
 if(NOT TARGET retdec::fileformat)
-    find_package(retdec @PROJECT_VERSION@
-        REQUIRED
-        COMPONENTS
-            crypto
-            common
-            utils
-            pelib
-            elfio
-            llvm
-    )
+	find_package(retdec @PROJECT_VERSION@
+		REQUIRED
+		COMPONENTS
+			crypto
+			common
+			utils
+			pelib
+			elfio
+			llvm
+			openssl-crypto
+	)
 
-    include(${CMAKE_CURRENT_LIST_DIR}/retdec-fileformat-targets.cmake)
+	include(${CMAKE_CURRENT_LIST_DIR}/retdec-fileformat-targets.cmake)
 endif()
