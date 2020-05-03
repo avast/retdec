6270442078e5d0357f4a241c92fb1eeae16c1d0e
Petr Zemek <petr.zemek@avast.com>
2020-05-03 10:54:17 +0200

    scripts: Remove a redundant underscore from a local variable in retdec-archive-decompiler.py.
    There is no need for it to end with `_`. The underscore makes its name less readable.

--- scripts/retdec-archive-decompiler.py
+++ scripts/retdec-archive-decompiler.py
@@ -182,13 +182,13 @@ class ArchiveDecompiler:
             # We have to use indexes instead of names because archives can contain multiple files with same name.
             log_file = self.library_path + '.file_' + str(file_index) + '.log.verbose'
 
-            arg_list_ = [sys.executable, config.DECOMPILER, '--ar-index=' + str(i), '-o',
+            arg_list = [sys.executable, config.DECOMPILER, '--ar-index=' + str(i), '-o',
                                                       self.library_path + '.file_' + str(file_index) + '.c',
                                                       self.library_path]
             if self.decompiler_args:
-                arg_list_.append(self.decompiler_args)
+                arg_list.append(self.decompiler_args)
             # Do not escape!
-            output, rc, timeouted = CmdRunner.run_cmd(arg_list_,
+            output, rc, timeouted = CmdRunner.run_cmd(arg_list,
                                                       timeout=self.timeout,
                                                       buffer_output=True)
 
