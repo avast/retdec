0a1c1eb20bcb084508877d89a1a6f2161b112119
Matula Peter <peter.matula@avast.com>
2020-07-21 13:49:03 +0200

    fileformat/retdec-fileformat-config.cmake: add OpenSSL remove retdec::crypto

--- src/fileformat/retdec-fileformat-config.cmake
+++ src/fileformat/retdec-fileformat-config.cmake
@@ -1,9 +1,14 @@
 
 if(NOT TARGET retdec::fileformat)
+    if(WIN32)
+        set(OPENSSL_USE_STATIC_LIBS TRUE)
+        set(OPENSSL_MSVC_STATIC_RT @RETDEC_MSVC_STATIC_RUNTIME@)
+    endif()
+    find_package(OpenSSL 1.0.1 REQUIRED)
+
     find_package(retdec @PROJECT_VERSION@
         REQUIRED
         COMPONENTS
-            crypto
             common
             utils
             pelib
