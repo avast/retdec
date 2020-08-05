7f807bdc20d066d28c3159be7a10ba1b5ede05c6
Peter Matula <peter.matula@avast.com>
2020-08-05 10:11:57 +0200

    cmake/deps.cmake: replace INTERNAL with STRING in set()
    INTERNAL implies FORCE and therefore rewrites user's variables. These variables were not meant to be changed by the user, but in some cases, if the users knows what they are doing, they could do so.

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,67 +1,67 @@
 
 set(CAPSTONE_URL
     "https://github.com/aquynh/capstone/archive/bc8a649b35188786754ea1b0bddd5cb48a039162.zip"
-    CACHE INTERNAL "URL of Capstone archive to use."
+    CACHE STRING "URL of Capstone archive to use."
 )
 set(CAPSTONE_ARCHIVE_SHA256
     "7d3075bce1f5622279c16a6f62fe8c548d4544bfc82292f6bf43907d0317fd10"
-    CACHE INTERNAL ""
+    CACHE STRING ""
 )
 
 set(GOOGLETEST_URL
     "https://github.com/google/googletest/archive/90a443f9c2437ca8a682a1ac625eba64e1d74a8a.zip"
-    CACHE INTERNAL "URL of Googletest archive to use."
+    CACHE STRING "URL of Googletest archive to use."
 )
 set(GOOGLETEST_ARCHIVE_SHA256
     "6fb9a49ad77656c860cfdafbb3148a91f076a3a8bda9c6d8809075c832549dd4"
-    CACHE INTERNAL ""
+    CACHE STRING ""
 )
 
 set(KEYSTONE_URL
     "https://github.com/keystone-engine/keystone/archive/d7ba8e378e5284e6384fc9ecd660ed5f6532e922.zip"
-    CACHE INTERNAL "URL of Keystone archive to use."
+    CACHE STRING "URL of Keystone archive to use."
 )
 set(KEYSTONE_ARCHIVE_SHA256
     "13bd00e062e9c778fe76aaab5c163348b3c9457c0e9b2a4c2fb3e2d8747694ca"
-    CACHE INTERNAL ""
+    CACHE STRING ""
 )
 
 set(LLVM_URL
     "https://github.com/avast/llvm/archive/1459e28198b64aaa2710eeacb9c90c18cfcf69a9.zip"
-    CACHE INTERNAL "URL of LLVM archive to use."
+    CACHE STRING "URL of LLVM archive to use."
 )
 set(LLVM_ARCHIVE_SHA256
     "54067d9019440ad9afe8a3114968d251ff7ef8cc94a765fe6d6f6e85981866ed"
-    CACHE INTERNAL ""
+    CACHE STRING ""
 )
 
 set(YARA_URL
     "https://github.com/VirusTotal/yara/archive/v4.0.1.zip"
-    CACHE INTERNAL "URL of Yara archive to use."
+    CACHE STRING "URL of Yara archive to use."
 )
 set(YARA_ARCHIVE_SHA256
     "4dcc6907b8537b67b52a61aa76f01196a4cc8e8e9f5fb6e4dd835692c2370e83"
-    CACHE INTERNAL ""
+    CACHE STRING ""
 )
 
 set(YARAMOD_URL
     "https://github.com/avast/yaramod/archive/94fc854153f48556087533616e3b0945d6f8023c.zip"
-    CACHE INTERNAL "URL of YaraMod archive to use."
+    CACHE STRING "URL of YaraMod archive to use."
 )
 set(YARAMOD_ARCHIVE_SHA256
     "f89546604a9617afb9c6cf8f241cb6dfb398e13b1c77a19f6193d037572b875c"
-    CACHE INTERNAL ""
+    CACHE STRING ""
 )
 
 set(SUPPORT_PKG_URL
     "https://github.com/avast/retdec-support/releases/download/2019-03-08/retdec-support_2019-03-08.tar.xz"
-    CACHE INTERNAL "URL of RetDec support package to use."
+    CACHE STRING "URL of RetDec support package to use."
 )
 set(SUPPORT_PKG_SHA256
     "629351609bca0f4b8edbd4e53789192305256aeb908e953f5546e121a911d54e"
-    CACHE INTERNAL ""
+    CACHE STRING ""
 )
 set(SUPPORT_PKG_VERSION
     "2019-03-08"
-    CACHE INTERNAL ""
+    CACHE STRING ""
 )
