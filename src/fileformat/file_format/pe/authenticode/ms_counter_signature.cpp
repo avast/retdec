138bc71f43f0b52607081e2943834b58bd38e93d
HoundThe <cgkajm@gmail.com>
2021-02-15 22:26:18 +0100

    Forgot to add ms_counter_signature files to git

---
+++ src/fileformat/file_format/pe/authenticode/ms_counter_signature.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file src/fileformat/file_format/pe/authenticode/ms_nested_signature.cpp
+ * @brief Representation of MsCounterSignature
+ * @copyright (c) 2020 Avast Software, licensed under the MIT license
+ */
+
+#include "ms_counter_signature.h"
+
+namespace authenticode {
+
+MsCounterSignature::MsCounterSignature(const std::vector<std::uint8_t>& data)
+	: pkcs7(nullptr, PKCS7_free), tstInfo(nullptr, TS_TST_INFO_free), signers(nullptr, sk_X509_free)
+{
+	pkcs7.reset(getPkcs7(data));
+	if (!pkcs7) {
+		throw std::runtime_error("Couldn't parse MsNestedSignature");
+	}
+
+	tstInfo.reset(PKCS7_to_TS_TST_INFO(pkcs7.get()));
+	if (!tstInfo) {
+		throw std::runtime_error("Couldn't parse MsNestedSignature");
+	}
+
+	const ASN1_TIME* raw_time = TS_TST_INFO_get_time(tstInfo.get());
+
+	if (raw_time) {
+		signTime = parseDateTime(raw_time);
+	}
+	signers.reset(PKCS7_get0_signers(pkcs7.get(), pkcs7->d.sign->cert, 0));
+	auto signerCount = sk_X509_num(signers.get());
+	if (signerCount != 1) {
+		throw std::runtime_error("Incorrect amount of signers, expected 1, has : " + std::to_string(signerCount));
+	}
+
+	signCert = sk_X509_value(signers.get(), 0);
+	certs = pkcs7->d.sign->cert;
+	TS_MSG_IMPRINT* imprint = TS_TST_INFO_get_msg_imprint(tstInfo.get());
+	ASN1_STRING* raw_digest = TS_MSG_IMPRINT_get_msg(imprint);
+	digest = bytesToHexString(raw_digest->data, raw_digest->length);
+}
+
+} // namespace authenticode
---
+++ src/fileformat/file_format/pe/authenticode/ms_counter_signature.h
@@ -0,0 +1,37 @@
+/**
+ * @file src/fileformat/file_format/pe/authenticode/ms_nested_signature.h
+ * @brief Representation of MsCounterSignature
+ * @copyright (c) 2020 Avast Software, licensed under the MIT license
+ */
+
+#pragma once
+
+#include "helper.h"
+
+#include <memory>
+#include <openssl/bio.h>
+#include <openssl/pkcs7.h>
+
+#include <cstdint>
+#include <openssl/ts.h>
+#include <string>
+#include <vector>
+
+namespace authenticode {
+
+class MsCounterSignature
+{
+	std::unique_ptr<PKCS7, decltype(&PKCS7_free)> pkcs7;
+	std::unique_ptr<TS_TST_INFO, decltype(&TS_TST_INFO_free)> tstInfo;
+	std::unique_ptr<STACK_OF(X509), decltype(&sk_X509_free)> signers;
+
+public:
+	const X509* signCert = nullptr;
+	const STACK_OF(X509)* certs = nullptr;
+	std::string signTime;
+	std::string digest;
+
+	MsCounterSignature(const std::vector<std::uint8_t>& data);
+};
+
+} // namespace authenticode
