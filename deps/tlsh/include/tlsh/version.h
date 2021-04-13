711463d13b64ec1bcae221d120a84ab5a74e777c
Peter Matula <peter.matula@avast.com>
2021-04-13 14:07:18 +0200

    deps/tlsh: refactor CMake

---
+++ deps/tlsh/include/tlsh/version.h
@@ -0,0 +1,23 @@
+
+#define VERSION_MAJOR 4
+#define VERSION_MINOR 2
+#define VERSION_PATCH 1
+
+// Default.
+#define TLSH_HASH "compact hash"
+#define BUCKETS_128
+// Possible option.
+//#define TLSH_HASH "min hash"
+//#define BUCKETS_48
+// Possible option.
+//#define TLSH_HASH "full hash"
+//#define BUCKETS_256
+
+// Default.
+#define TLSH_CHECKSUM "1 byte checksum"
+// Possible option.
+//#define TLSH_CHECKSUM "no checksum"
+//#define CHECKSUM_0B
+// Possible option.
+//#define TLSH_CHECKSUM "3 bytes checksum"
+//#define CHECKSUM_3B
--- deps/tlsh/retdec-tlsh-config.cmake
+++ deps/tlsh/retdec-tlsh-config.cmake
@@ -1,3 +1,4 @@
+
 if(NOT TARGET retdec::deps::tlsh)
     include(${CMAKE_CURRENT_LIST_DIR}/retdec-tlsh-targets.cmake)
 endif()
