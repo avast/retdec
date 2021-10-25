bf1a566eebcc69345fdb9009e48cd96f16db4751
HoundThe <karel.hajek@avast.com>
2021-10-25 10:22:49 +0200

    Integrate new authenticode parser (#1027)
    * Integrate new authenticode-parser
    
    * Add comments
    
    * Integrate authenticode-parser repository as dependency
    
    * Update to new authenticode-parser version
    
    * Change the verification flow
    
    Co-authored-by: Peter Matula <peter.matula@avast.com>

--- deps/authenticode-parser/src/structs.c
+++ deps/authenticode-parser/src/structs.c
@@ -1,19 +1,25 @@
-/**
- * @file src/fileformat/file_format/pe/authenticode/authenticode_structs.cpp
- * @brief Defines custom OpenSSL objects and functions
- * @copyright (c) 2021 Avast Software, licensed under the MIT license
- * @author Marek Milkovič - metthal
- */
+/* Copyright (c) 2021 Avast Software
 
-/* Author #Metthal */
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
 
-#include "authenticode_structs.h"
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-/* 
-   These are types from "Windows Authenticode Portable Executable Signature Format"
-   https://download.microsoft.com/download/9/c/5/9c5b2167-8017-4bae-9fde-d599bac8184a/Authenticode_PE.docx
-   Some of them are changed a little bit because the documentation did not reflect the reality
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
+
+#include "structs.h"
 
 ASN1_CHOICE(SpcString) = {
 	ASN1_IMP_OPT(SpcString, value.unicode, ASN1_BMPSTRING, 0),
