b1613ef76201431601cad20bfb8518ebfb3aed30
Peter Kubov <peter.kubov@avast.com>
2022-01-18 12:38:31 +0100

    Try to fix win & mac tests

--- .github/workflows/macOS/run-tests.sh
+++ .github/workflows/macOS/run-tests.sh
@@ -8,6 +8,18 @@ clang_dir = $PWD/clang
 tests_root_dir = $PWD/retdec-regression-tests
 ; Path to the RetDec's installation directory.
 retdec_install_dir = $PWD/install
+
+; 2019-09-05: On macOS, we have to skip tests that compile output C files
+; because newer Xcode versions no longer support compilation into 32b binaries:
+;
+; "The macOS 10.14 SDK no longer contains support for compiling 32-bit applications.
+;  If developers need to compile for i386, Xcode 9.4 or earlier is required."
+;
+; We cannot compile into 64b binaries because of RetDec shortcomings
+; (https://github.com/avast/retdec/issues/213). So, we decided to skip tests that
+; compile output C files when running regression tests on macOS.
+skip_c_compilation_tests = 0
+
 EOF
 
 cd "$PWD/retdec-regression-tests-framework"
