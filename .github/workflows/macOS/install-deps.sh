fc6583fec285660ebda587b71864d6d679bfeff3
Karel Hájek <karel.hajek@gendigital.com>
2023-07-15 14:59:18 +0200

    Use new version of python that is not deprecated

--- .github/workflows/macOS/install-deps.sh
+++ .github/workflows/macOS/install-deps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-brew install pkg-config autoconf automake libtool openssl python@3.7
-brew link --overwrite python@3.7
+brew install pkg-config autoconf automake libtool openssl python@3.10
+brew link --overwrite python@3.10
