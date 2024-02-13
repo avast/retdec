e08b0a573f5784515940f4f9415ebe637a5455bf
Marek Milkovič <marek.milkovic@gendigital.com>
2024-02-13 19:15:08 +0100

    Fixed path in Windows CI build script

--- .github/workflows/Windows/install-deps.sh
+++ .github/workflows/Windows/install-deps.sh
@@ -8,4 +8,4 @@ choco install --no-progress openssl 7zip
 # FindOpenSSL.cmake in the version we have available. This moves the files
 # where the finder expects them. Otherwise mismatch between OpenSSL versions
 # can and will happen.
-cp "/c/Program Files/lib/VC/x64/MD/*" "/c/Program Files/lib/VC/x64/"
+cp "/c/Program Files/OpenSSL/lib/VC/x64/MD/*" "/c/Program Files/OpenSSL/lib/VC/x64/"
