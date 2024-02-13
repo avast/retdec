86150131a3e5ed6ae8dda737a6e34220d23ed65c
Marek Milkovič <marek.milkovic@gendigital.com>
2024-02-13 19:03:52 +0100

    Fix of github workflows on windows
    There is a mismatch of OpenSSL versions being used for headers vs.
    linked against

--- .github/workflows/Windows/install-deps.sh
+++ .github/workflows/Windows/install-deps.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/bash
 
+rm -rf "/c/Program Files/OpenSSL"
+
 choco install --no-progress openssl 7zip
+
+# Chocolatey installs OpenSSL into subfolder MD which isn't found with
+# FindOpenSSL.cmake in the version we have available. This moves the files
+# where the finder expects them. Otherwise mismatch between OpenSSL versions
+# can and will happen.
+cp "/c/Program Files/lib/VC/x64/MD/*" "/c/Program Files/lib/VC/x64/"
