8756d4ba530b3b36ba352b54d7d4a52c3a73a71c
Marek Milkovič <marek.milkovic@gendigital.com>
2024-02-13 20:17:06 +0100

    Debug github action workflow

--- .github/workflows/Windows/install-deps.sh
+++ .github/workflows/Windows/install-deps.sh
@@ -8,4 +8,11 @@ choco install --no-progress openssl 7zip
 # FindOpenSSL.cmake in the version we have available. This moves the files
 # where the finder expects them. Otherwise mismatch between OpenSSL versions
 # can and will happen.
+set -x
+ls -la "/c/Program Files/"
+ls -la "/c/Program Files/OpenSSL/"
+ls -la "/c/Program Files/OpenSSL/lib/"
+ls -la "/c/Program Files/OpenSSL/lib/VC/"
+ls -la "/c/Program Files/OpenSSL/lib/VC/x64/"
+ls -la "/c/Program Files/OpenSSL/lib/VC/x64/MD/"
 cp "/c/Program Files/OpenSSL/lib/VC/x64/MD/*" "/c/Program Files/OpenSSL/lib/VC/x64/"
