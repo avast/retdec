3ac68bbf8583f769f6535046faab22e765b6d664
Marek Milkovič <marek.milkovic@gendigital.com>
2025-05-25 09:00:37 +0200

    ci: Build still on unbuntu 22.04 because there's LLVM dependency on libtinfo5 which isn't available in 24.04 anymore

--- .github/workflows/Linux/install-deps.sh
+++ .github/workflows/Linux/install-deps.sh
@@ -3,6 +3,6 @@
 set -x
 
 sudo apt-get update
-sudo apt-get install openssl gcc-multilib python3-venv doxygen graphviz libncurses6
+sudo apt-get install openssl gcc-multilib python3-venv doxygen graphviz libncurses5
 
 doxygen --version
